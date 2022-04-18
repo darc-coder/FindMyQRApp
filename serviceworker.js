2c266faecd35d0b785d0627430cda60e9e08ce9f
Nitin Lakra <nitin.lakra@netcracker.com>
2022-04-18 13:59:57 +0530

    Final Version 1.0

--- serviceworker.js
+++ serviceworker.js
@@ -14,6 +14,7 @@ const assets = [
     '/resources/MaterialIcons-Regular.ttf',
     '/resources/MaterialIconsOutlined-Regular.otf',
     '/resources/bg.jpg',
+    '/resources/astronaut.png',
     '/resources/FindMyQRLogo.png',
     '/html/fallback.html'
 ];
